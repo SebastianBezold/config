0ac85fd5184fd9e3aa6f592375a55447bb9bb44b
Sebastian Bezold <SebastianBezold@gmail.com>
2025-07-15 22:50:43 +0200

    remove non-dotfile things

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -17,13 +17,13 @@ return {
 		"neovim/nvim-lspconfig",
 		lazy = false,
 		config = function()
-			local cmp_nvim_lsp = require("cmp_nvim_lsp")
-			local capabilities = vim.tbl_deep_extend(
-				"force",
-				{},
-				vim.lsp.protocol.make_client_capabilities(),
-				cmp_nvim_lsp.default_capabilities()
-			)
+		--	local cmp_nvim_lsp = require("cmp_nvim_lsp")
+		--	local capabilities = vim.tbl_deep_extend(
+		--		"force",
+		--		{},
+		--		vim.lsp.protocol.make_client_capabilities(),
+		--		cmp_nvim_lsp.default_capabilities()
+		--	)
 
 			local lspconfig = require("lspconfig")
 		end,
