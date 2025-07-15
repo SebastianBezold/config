237ba601b1cfe8e25704f6c4c24684300fc592b2
Sebastian Bezold <SebastianBezold@gmail.com>
2025-07-15 21:51:21 +0200

    add a bunch of aliases to .zshrc

---
+++ zsh/.config/zsh/aliases.zsh
@@ -0,0 +1,24 @@
+# Git aliases
+
+alias gs="git status"
+alias gp="git push"
+alias gcb="git checkout -b"
+
+
+# GNU aliases
+# Overwriting a couple of tools to the GNU version to be more compatible with colleagues
+
+alias sed="gnu-sed"
+alias tar="gnu-tar"
+
+
+# CLI tool shortcuts
+
+alias lg="lazygit"
+alias vim="nvim"
+
+
+# config shortcuts
+
+alias zconfig="nvim $HOME/.zshrc"
+alias zreload="source $HOME/.zshrc"
---
+++ zsh/.zshrc
@@ -0,0 +1,7 @@
+export EDITOR="nvim"
+
+# Load additional zsh config
+for conf in "$HOME/.config/zsh/"*.zsh; do
+  source "${conf}"
+done
+unset conf
